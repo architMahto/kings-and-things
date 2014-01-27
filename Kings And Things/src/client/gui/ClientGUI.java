13e16a5c10fd329673ab55789a28dff40c497fbb
sh.emami65@gmail.com <sh.emami65@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-01-27 03:40:31 +0000

    added all resources to SVN, add GUI for Board and Hex to client, no function yet

--- Kings And Things/src/client/gui/ClientGUI.java
+++ Kings And Things/src/client/gui/ClientGUI.java
@@ -5,9 +5,9 @@ import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowAdapter;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,16 +16,19 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JScrollPane;
 
-import common.Constants.Level;
 import common.Logger;
 import common.Console;
+import common.Constants.Level;
 import common.event.EventHandler;
 import common.event.EventMonitor;
 import common.network.Connection;
 import static common.Constants.CONSOLE;
 import static common.Constants.SERVER_IP;
+import static common.Constants.BOARD_SIZE;
 import static common.Constants.SERVER_PORT;
 import static common.Constants.CONSOLE_SIZE;
+import static common.Constants.IP_COLUMN_COUNT;
+import static common.Constants.PORT_COLUMN_COUNT;
 
 /**
  * client GUI to hold all and display all game related information
@@ -73,6 +76,28 @@ public class ClientGUI extends JFrame implements Runnable, EventHandler{
 		JPanel jpMain = new JPanel( new GridBagLayout());
 		GridBagConstraints constraints = new GridBagConstraints();
 		constraints.fill = GridBagConstraints.BOTH;
+		//constraints.insets = new Insets( 5, 5, 5, 5);
+
+		Board board = new Board( null, true);
+		board.setPreferredSize( BOARD_SIZE);
+		board.init();
+		constraints.gridx = 0;
+		constraints.gridy = 0;
+		jpMain.add( board, constraints);
+
+		/*constraints.weighty = 1;
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.gridwidth = GridBagConstraints.REMAINDER;
+		jpMain.add( createTempPanel(), constraints);*/
+		
+		return jpMain;
+	}
+	
+	private JPanel createTempPanel(){
+		JPanel jpMain = new JPanel( new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.BOTH;
 		constraints.insets = new Insets( 5, 5, 5, 5);
 
 		JLabel label = new JLabel( "Input:");
@@ -90,6 +115,46 @@ public class ClientGUI extends JFrame implements Runnable, EventHandler{
 		constraints.gridy = 0;
 		constraints.gridwidth = GridBagConstraints.REMAINDER;
 		jpMain.add( jtfInput, constraints);
+		
+		label = new JLabel( "IP:");
+		constraints.weightx = 0;
+		constraints.weighty = 0;
+		constraints.gridx = 0;
+		constraints.gridy = 1;
+		constraints.gridwidth = 1;
+		jpMain.add( label, constraints);
+		
+		jtfIP = new JTextField( SERVER_IP, IP_COLUMN_COUNT);
+		constraints.weightx = 1;
+		constraints.weighty = 0;
+		constraints.gridx = 1;
+		constraints.gridy = 1;
+		constraints.gridwidth = 2;
+		jpMain.add( jtfIP, constraints);
+		
+		label = new JLabel( "Port:");
+		constraints.weightx = 0;
+		constraints.weighty = 0;
+		constraints.gridx = 3;
+		constraints.gridy = 1;
+		constraints.gridwidth = 1;
+		jpMain.add( label, constraints);
+		
+		jtfPort = new JTextField( SERVER_PORT+"", PORT_COLUMN_COUNT);
+		constraints.weightx = .5;
+		constraints.gridwidth = 1;
+		constraints.gridx = 4;
+		constraints.gridy = 1;
+		jpMain.add( jtfPort, constraints);
+		
+		JButton jbConnect = new JButton( "Connect   ");
+		jbConnect.setActionCommand( "Connection");
+		jbConnect.addActionListener( control);
+		constraints.weightx = 0;
+		constraints.gridwidth = 1;
+		constraints.gridx = 5;
+		constraints.gridy = 1;
+		jpMain.add( jbConnect, constraints);
 
 		console = new Console();
 		console.setEditable( false);
@@ -100,57 +165,8 @@ public class ClientGUI extends JFrame implements Runnable, EventHandler{
 		constraints.gridwidth = GridBagConstraints.REMAINDER;
 		constraints.weighty = 1;
 		constraints.gridx = 0;
-		constraints.gridy = 1;
+		constraints.gridy = 2;
 		jpMain.add( jsp, constraints);
-		
-		label = new JLabel( "IP:");
-		constraints.weightx = 0;
-		constraints.weighty = 0;
-		constraints.gridx = 0;
-		constraints.gridy = 2;
-		constraints.gridwidth = 1;
-		jpMain.add( label, constraints);
-		
-		jtfIP = new JTextField( SERVER_IP);
-		constraints.weightx = 1;
-		constraints.weighty = 0;
-		constraints.gridx = 1;
-		constraints.gridy = 2;
-		constraints.gridwidth = 2;
-		jpMain.add( jtfIP, constraints);
-		
-		label = new JLabel( "Port:");
-		constraints.weightx = 0;
-		constraints.weighty = 0;
-		constraints.gridx = 3;
-		constraints.gridy = 2;
-		constraints.gridwidth = 1;
-		jpMain.add( label, constraints);
-		
-		jtfPort = new JTextField( SERVER_PORT+"");
-		constraints.weightx = .5;
-		constraints.gridwidth = 1;
-		constraints.gridx = 4;
-		constraints.gridy = 2;
-		jpMain.add( jtfPort, constraints);
-		
-		JButton jbConnect = new JButton( "Connect");
-		jbConnect.setActionCommand( "Connect");
-		jbConnect.addActionListener( control);
-		constraints.weightx = 0;
-		constraints.gridwidth = 1;
-		constraints.gridx = 5;
-		constraints.gridy = 2;
-		jpMain.add( jbConnect, constraints);
-		
-		JButton jbClose = new JButton( "Disconnect");
-		jbClose.setActionCommand( "Disconnect");
-		jbClose.addActionListener( control);
-		constraints.weightx = 0;
-		constraints.gridwidth = 1;
-		constraints.gridx = 6;
-		constraints.gridy = 2;
-		jpMain.add( jbClose, constraints);
 		
 		return jpMain;
 	}
@@ -160,7 +176,9 @@ public class ClientGUI extends JFrame implements Runnable, EventHandler{
 		@Override
 		public void windowClosed(WindowEvent e){
 			connection.disconnect();
-			Logger.flush( "Client", console.getText());
+			if( console!=null){
+				Logger.flush( "Client", console.getText());
+			}
 		}
 	}
 	
@@ -168,8 +186,16 @@ public class ClientGUI extends JFrame implements Runnable, EventHandler{
 
 		@Override
 		public void actionPerformed( ActionEvent e) {
-			if( e.getActionCommand().equals( "Connect")){
-				connection.connectTo( jtfIP.getText(), Integer.parseInt( jtfPort.getText()));
+			if( e.getActionCommand().equals( "Connection")){
+				JButton source = (JButton)e.getSource();
+				if (connection.isConnected()){
+					connection.disconnect();
+					source.setText( "Connect   ");
+				}else{
+					if(	connection.connectTo( jtfIP.getText(), Integer.parseInt( jtfPort.getText()))){
+						source.setText( "Disconnect");
+					}
+				}
 			}else if( e.getActionCommand().equals( "Input")){
 				JTextField source = (JTextField)e.getSource();
 				String input = source.getText();
@@ -178,14 +204,14 @@ public class ClientGUI extends JFrame implements Runnable, EventHandler{
 					connection.recieve();
 					source.setText( "");
 				}
-			}else{
-				connection.disconnect();
 			}
 		}
 	}
 
 	@Override
 	public void handel( String message, Level level) {
-		console.add( message, level);
+		if( console!=null){
+			console.add( message, level);
+		}
 	}
 }
--- Kings And Things/src/common/Constants.java
+++ Kings And Things/src/common/Constants.java
@@ -2,11 +2,15 @@ package common;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.image.BufferedImage;
 
 public interface Constants {
 	
 	enum Level { Error, Warning, Notice, Plain, END}
 
+	//Hex images
+	public static final BufferedImage[] HEX_IMAGES = new BufferedImage[9];
+	
 	//Colors
 	public static final Color RED = new Color( 255, 0, 0);
 	public static final Color DARK_RED = new Color( 128, 0, 0);
@@ -32,19 +36,43 @@ public interface Constants {
 	public static final int PLAYER_INC = 16;  //00010000
 	
 	//Maximums
+	public static final int MAX_HEXES = 48;
 	public static final int MAX_PLAYERS = 4;
+	public static final int MAX_HEXES_ON_BOARD = 37;
 	
 	//Minimums
 	public static final int MIN_PLAYERS = 2;
+	public static final int MIN_HEXES_ON_BOARD = 19;
 	
 	//Sizes
+	public static final int LOCK_SIZE = 28;
+	public static final int HEX_SPACING = 16;
 	public static final int LABEL_FONT_SIZE = 25;
+	public static final int IP_COLUMN_COUNT = 12;
 	public static final int PLAYER_FONT_SIZE = 12;
+	public static final int PORT_COLUMN_COUNT = 7;
+	public static final int BOARD_TOP_PADDING = 100;
+	public static final int BOARD_WIDTH_SEGMENT = 8;
+	public static final int BOARD_PLAYERS_STATE = 450;
+	public static final int BOARD_HEIGHT_SEGMENT = 14;
+	public static final double HEX_RATIO = 752.0/658.0;
+	public static final int BOARD_BOTTOM_PADDING = 120;
 	public static final Dimension LABEL_SIZE = new Dimension( 50,50);
-	public static final Dimension CONSOLE_SIZE = new Dimension( 400,150);
+	public static final Dimension CONSOLE_SIZE = new Dimension( 100,75);
+	public static final Dimension HEX_SIZE = new Dimension( (int)(90*HEX_RATIO),90);
+	public static final Dimension HEX_BOARD_SIZE = new Dimension( HEX_SIZE.height*BOARD_HEIGHT_SEGMENT/2 + HEX_SPACING, HEX_SIZE.height*BOARD_HEIGHT_SEGMENT/2 + HEX_SPACING);
+	public static final Dimension BOARD_SIZE = new Dimension( HEX_BOARD_SIZE.width + BOARD_PLAYERS_STATE, HEX_BOARD_SIZE.height + BOARD_BOTTOM_PADDING);
 	
 	//Defaults
+	public static final int SPIRAL_DELAY = 3;
 	public static final int SERVER_TIMEOUT = 10;
 	public static final int SERVER_PORT = 12345;
+	public static final int HEX_MOVE_DISTANCE = 15;
 	public static final String SERVER_IP = "127.0.0.1";
+	public static final int BOARD_LOAD_ROW[][] = { { 7, 5, 6, 8, 9, 8, 6},
+													{4, 3, 4, 5, 7, 9, 10, 11, 10, 9, 7, 5},
+													{3, 2, 1, 2, 3, 4, 6, 8, 10, 11, 12, 13, 12, 11, 10, 8, 6, 4}};
+	public static final int BOARD_LOAD_COL[][] = { { 4, 4, 5, 5, 4, 3, 3},
+													{3, 4, 5, 6, 6, 6, 5, 4, 3, 2, 2, 2},
+													{2, 3, 4, 5, 6, 7, 7, 7, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1}};
 }
