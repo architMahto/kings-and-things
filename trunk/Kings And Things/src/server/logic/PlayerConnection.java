bcaa09fc0f34010fbda254a0085f497bdd83cb63
ghost3141592@gmail.com <ghost3141592@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-02-04 18:35:14 +0000

    Add new message for single client requesting to start the game
    Fix the StartGameCommand

--- trunk/Kings And Things/src/server/logic/PlayerConnection.java
+++ trunk/Kings And Things/src/server/logic/PlayerConnection.java
@@ -7,6 +7,7 @@ import common.event.CommandEventBus;
 import common.event.EventMonitor;
 import common.event.SendCommandAcrossNetworkEvent;
 import common.game.CommandMarshaller;
+import common.game.Player;
 import common.network.Connection;
 import static common.Constants.CONSOLE;
 
@@ -14,15 +15,49 @@ public class PlayerConnection extends Thread{
 	
 	private final int PLAYER_ID;
 	private Connection connection;
+	private boolean readyToStart;
+	private String playerName;
 	
 	public PlayerConnection( final int PLAYER_ID, Connection connection){
 		this.PLAYER_ID = PLAYER_ID;
 		this.connection = connection;
+		readyToStart = false;
+		playerName = "Player " + PLAYER_ID;
 	}
 	
 	public void initialize()
 	{
 		CommandEventBus.BUS.register(this);
+	}
+	
+	public boolean isReadyToStart()
+	{
+		return readyToStart;
+	}
+	
+	public void setReadyToStart(boolean newVal)
+	{
+		readyToStart = newVal;
+	}
+	
+	public int getPlayerId()
+	{
+		return PLAYER_ID;
+	}
+	
+	public String getPlayerName()
+	{
+		return playerName;
+	}
+	
+	public void setPlayerName(String newName)
+	{
+		playerName = newName;
+	}
+	
+	public Player toPlayerObj()
+	{
+		return new Player(playerName, PLAYER_ID);
 	}
 	
 	@Override
