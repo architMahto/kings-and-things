8a02b7f41b40756fecb97545778378e80b704d6f
sh.emami65@gmail.com <sh.emami65@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-02-05 23:36:33 +0000

    removed all references and classes of old event system (EventMonitor and EventHandler)

--- Kings And Things/src/common/Constants.java
+++ Kings And Things/src/common/Constants.java
@@ -58,12 +58,6 @@ public final class Constants {
 	public static final Color COLOR_WARNNING = DARK_YELLOW;
 	
 	//keys for events
-	public static final int HOLD = 0;         //00000000
-	public static final int WAIT = 1;         //00000001
-	public static final int UPDATE = 2;       //00000010
-	public static final int ANALYSE = 3;      //00000011
-	public static final int CONSOLE = 4;      //00000100
-	public static final int ENDGAME = 5;      //00000101
 	public static final int PLAYER = 128;     //10000000
 	public static final int PLAYER_INC = 16;  //00010000
 	
