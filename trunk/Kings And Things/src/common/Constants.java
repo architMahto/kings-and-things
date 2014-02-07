d93ab14fdd393a8fb34b88669e9c0f0cac9f5f4a
sh.emami65@gmail.com <sh.emami65@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-02-07 22:35:59 +0000

    updated guava to 16.0.1

--- trunk/Kings And Things/src/common/Constants.java
+++ trunk/Kings And Things/src/common/Constants.java
@@ -6,8 +6,6 @@ import javax.imageio.ImageIO;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.Polygon;
