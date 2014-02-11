4bf5147235b62aea5e9451b99de00288eea8958a
ghost3141592@gmail.com <ghost3141592@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-02-11 23:55:00 +0000

    Add JPanel for displaying a hex's contents

---
+++ trunk/Kings And Things/src/client/gui/HexContentsPanel.java
@@ -0,0 +1,40 @@
+package client.gui;
+
+import java.awt.FlowLayout;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import common.Constants;
+import common.game.HexState;
+import common.game.TileProperties;
+
+public class HexContentsPanel extends JPanel
+{
+	private static final long serialVersionUID = 7410672134640031418L;
+	
+	private HexState model;
+	
+	/**
+	 * Simple panel for displaying a hex's contents
+	 * @param model The hex to display contents of
+	 */
+	public HexContentsPanel(HexState model)
+	{
+		this.model = model;
+		init();
+	}
+	
+	private void init()
+	{
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		
+		for(TileProperties tp : model.getThingsInHex())
+		{
+			Image tileImage = Constants.IMAGES.get(tp.hashCode()).getScaledInstance(Constants.TILE_SIZE.width, Constants.TILE_SIZE.height, Image.SCALE_DEFAULT);
+			add(new JLabel(new ImageIcon(tileImage)));
+		}
+	}
+}
