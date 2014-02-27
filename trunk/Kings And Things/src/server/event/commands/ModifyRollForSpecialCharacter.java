fe1173247afd9e042929e69c88d39940f7f0fba1
architmahto@gmail.com <architmahto@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-02-27 21:11:24 +0000

    Created new class to notify server about modifying a dice roll for special character

---
+++ trunk/Kings And Things/src/server/event/commands/ModifyRollForSpecialCharacter.java
@@ -0,0 +1,25 @@
+package server.event.commands;
+
+public class ModifyRollForSpecialCharacter extends AbstractCommand {
+	
+	private final boolean hasRolled; 
+	private final int goldAmount;
+	
+	public ModifyRollForSpecialCharacter (boolean hasRolled, int goldAmount) {
+		this.hasRolled = hasRolled;
+		this.goldAmount = goldAmount;
+	}
+	
+	/*Getter Methods*/
+	
+	// checks if the player has rolled
+	public boolean checkRolled () {
+		return this.hasRolled;
+	}
+	
+	// gets the amount of gold the player wants to pay
+	public int retrieveGoldAmount () {
+		return this.goldAmount;
+	}
+
+}
