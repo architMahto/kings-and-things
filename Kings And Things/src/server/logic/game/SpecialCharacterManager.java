c488743b18a167aaf5d1eabfedba04e2ff4498c7
ghost3141592@gmail.com <ghost3141592@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-02-27 20:32:05 +0000

    Make method in TileProperties protected, add constant mapping for keeping track of paired heroes, add class for managing special characters, fix typo in tile name

---
+++ Kings And Things/src/server/logic/game/SpecialCharacterManager.java
@@ -0,0 +1,15 @@
+package server.logic.game;
+
+import java.util.Collection;
+
+import common.game.TileProperties;
+
+public class SpecialCharacterManager extends AbstractTileManager
+{
+	
+	protected SpecialCharacterManager(Collection<? extends TileProperties> tiles, String tileType)
+	{
+		super(tiles, tileType);
+	}
+
+}
