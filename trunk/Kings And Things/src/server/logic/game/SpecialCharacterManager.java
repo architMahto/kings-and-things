ff95cfd49588cfcc81ad72faa86978fd131552b5
ghost3141592@gmail.com <ghost3141592@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-02-27 20:56:20 +0000

    Remove TODOs from TwoSidedTileProperties, implement hashcode, equals, and toString, implement logic for SpecialCharacterManager class

--- trunk/Kings And Things/src/server/logic/game/SpecialCharacterManager.java
+++ trunk/Kings And Things/src/server/logic/game/SpecialCharacterManager.java
@@ -1,15 +1,40 @@
 package server.logic.game;
 
 import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import common.Constants;
 import common.game.TileProperties;
+import common.game.TwoSidedTileProperties;
 
 public class SpecialCharacterManager extends AbstractTileManager
 {
-	
-	protected SpecialCharacterManager(Collection<? extends TileProperties> tiles, String tileType)
+	protected SpecialCharacterManager(Collection<? extends TileProperties> tiles)
 	{
-		super(tiles, tileType);
+		super(getSpecialCharacterSetFromCollection(tiles), "Special Character");
 	}
 
+	private static Set<TwoSidedTileProperties> getSpecialCharacterSetFromCollection(Collection<? extends TileProperties> tiles)
+	{
+		LinkedHashSet<TwoSidedTileProperties> heroes = new LinkedHashSet<TwoSidedTileProperties>();
+		for(TileProperties tp : tiles)
+		{
+			if(Constants.HERO_PAIRINGS.containsKey(tp.getName()))
+			{
+				TileProperties correspondingTile = null;
+				for(TileProperties tp2 : tiles)
+				{
+					if(tp2.getName().equals(Constants.HERO_PAIRINGS.get(tp.getName())))
+					{
+						correspondingTile = tp2;
+						break;
+					}
+				}
+				heroes.add(new TwoSidedTileProperties(tp,correspondingTile));
+			}
+		}
+		
+		return heroes;
+	}
 }
