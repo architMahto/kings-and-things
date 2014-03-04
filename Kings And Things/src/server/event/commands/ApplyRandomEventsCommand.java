8fee54c80f9392e62029705bb5c959365334b96f
architmahto@gmail.com <architmahto@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-03-04 20:54:21 +0000

    Added new classes for Random Events phase

---
+++ Kings And Things/src/server/event/commands/ApplyRandomEventsCommand.java
@@ -0,0 +1,22 @@
+package server.event.commands;
+
+import common.game.ITileProperties;
+
+public class ApplyRandomEventsCommand extends AbstractCommand {
+	
+	private final ITileProperties eventOfPlayer;
+	
+	
+	// constructor
+	public ApplyRandomEventsCommand (ITileProperties eventOfPlayer) {
+		this.eventOfPlayer = eventOfPlayer;
+	}
+	
+	/*Getter Methods*/
+	
+	// retrieves event of player
+	public ITileProperties getEventOfPlayer () {
+		return eventOfPlayer;
+	}
+
+}
