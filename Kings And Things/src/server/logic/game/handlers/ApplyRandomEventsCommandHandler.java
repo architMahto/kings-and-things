3194abfbe8a222634f7d71f1915d9e0f3dbc0fd1
architmahto@gmail.com <architmahto@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-03-04 21:16:42 +0000

    Added skeleton to ApplyRandomEventsCommandHandler

--- Kings And Things/src/server/logic/game/handlers/ApplyRandomEventsCommandHandler.java
+++ Kings And Things/src/server/logic/game/handlers/ApplyRandomEventsCommandHandler.java
@@ -1,5 +1,30 @@
 package server.logic.game.handlers;
 
-public class ApplyRandomEventsCommandHandler extends CommandHandler {
+import server.event.commands.ApplyRandomEventsCommand;
 
+import com.google.common.eventbus.Subscribe;
+
+import common.Logger;
+import common.game.ITileProperties;
+
+public class ApplyRandomEventsCommandHandler extends CommandHandler {
+	
+	public void applyRandomEventEffect (ITileProperties randomEventTile) {
+		
+		String randomEventName = randomEventTile.getName();
+		
+		switch (randomEventName) {
+		    
+		}
+	}
+	
+	
+	@Subscribe
+	public void receiveApplyEventsCommand (ApplyRandomEventsCommand randomEvent) {
+		try {
+			
+		} catch (Throwable t) {
+			Logger.getErrorLogger().error("Unable to apply random event due to: ");
+		}
+	}
 }
