8c7dd4fd8a324c673dd433dc36ba1d8977a3c3d0
sh.emami65@gmail.com <sh.emami65@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-03-26 02:33:11 +0000

    added toString to Roll and DieRoll
    added Logger for any unsupported events
    
    updated DiceRoller to display correct result
    
    added support to ignore Rolls till correct size is received

--- Kings And Things/src/common/event/network/DieRoll.java
+++ Kings And Things/src/common/event/network/DieRoll.java
@@ -6,16 +6,21 @@ import common.game.Roll;
 public class DieRoll extends AbstractNetwrokEvent {
 	
 	private static final long serialVersionUID = 1732457814465837071L;
-	private Roll dieRoll;
+	private Roll roll;
 	
-	public DieRoll(Roll dieRoll) {
-		this.dieRoll = dieRoll;
+	public DieRoll(Roll roll) {
+		this.roll = roll;
 	}
 	
 	/**
 	 * Retrieves the die roll to notify the client
 	 */
 	public Roll getDieRoll() {
-		return dieRoll;
+		return roll;
+	}
+	
+	@Override
+	public String toString(){
+		return "Network/DieRoll: " + roll;
 	}
 }
