e74102b8c10ed850218c323022610e1ddc392cfe
sh.emami65@gmail.com <sh.emami65@gmail.com@0f1e8298-1b2a-6a4c-d5cb-7e6b15c73b17>
2014-04-05 20:59:01 +0000

    more adjustments to undo and mouse movements

--- Kings And Things/src/client/gui/util/undo/UndoTileMovement.java
+++ Kings And Things/src/client/gui/util/undo/UndoTileMovement.java
@@ -12,17 +12,55 @@ public class UndoTileMovement implements Undo{
 	private HexState state;
 	private Point location;
 	
-	public UndoTileMovement( Tile tile, Point location){
+	private UndoTileMovement( Tile tile, HexState hex, Point location){
 		this.tile = tile;
+		this.state = hex;
 		this.location = location;
 	}
 
+	/**
+	 * used when initial mouse press happens
+	 */
+	public UndoTileMovement( Tile tile, Point location){
+		this(tile, null, location);
+	}
+
+	/**
+	 * used when placing marker on hex
+	 */
+	public UndoTileMovement( Tile tile, HexState hex) {
+		this(tile, hex, null);
+	}
+	
+	/**
+	 * used when exchanging hex tiles
+	 */
+	public UndoTileMovement( Tile tile) {
+		this(tile, null, null);
+	}
+
 	@Override
-	public void undo( MoveAnimation animation) {
+	public void undo( MoveAnimation animation, Parent parent) {
 		if( animation==null){
 			throw new NullPointerException();
 		}
-		tile.setDestination( location);
-		animation.start( tile);
+		if( state==null && location==null){
+			if( tile.getParent()==null){
+				parent.addTile(tile);
+			}
+		}else if( location==null){
+			state.removeMarker();
+			if( tile.getParent()==null){
+				parent.addTile(tile);
+			}
+		}else if( state==null){
+			tile.setDestination( location);
+			animation.start( tile);
+		}
+	}
+
+	@Override
+	public boolean undoLast() {
+		return location==null || ( state==null && location==null);
 	}
 }
